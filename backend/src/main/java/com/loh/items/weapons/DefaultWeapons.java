ab7a9c3635252e01ba4d5a0362858b0fe98395ee
Ramiro <ramiro.tyszka@gmail.com>
2020-03-04 14:55:18 -0300

    #6 added seeder to dummy weapons

---
+++ backend/src/main/java/com/loh/items/weapons/DefaultWeapons.java
@@ -0,0 +1,13 @@
+package com.loh.items.weapons;
+
+public class DefaultWeapons {
+
+    public static String[] lightWeapons = new String[] {"Dagger"};
+    public static String[] mediumWeapons = new String[] {"Long Sword"};
+    public static String[] heavyWeapons = new String[] {"Great Axe"};
+
+    public static String dummyLightWeapon = "Dummy Light Weapon";
+    public static String dummyMediumWeapon = "Dummy Medium Weapon";
+    public static String dummyHeavyWeapon = "Dummy Heavy Weapon";
+    public static String dummyNoneWeapon = "Dummy None Weapon";
+}
