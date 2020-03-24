e396aa10cc93d86ccc353debc04757ff51108c12
Ramiro <ramiro.tyszka@gmail.com>
2020-03-24 13:23:57 -0300

    #32 added isItem method

--- web/src/app/shared/utils/isItem.ts
+++ web/src/app/shared/utils/isItem.ts
@@ -5,9 +5,11 @@ import { WeaponInstance } from '../models/WeaponInstance.model';
 import {GloveInstance} from '../models/GloveInstance.model';
 import {BeltInstance} from '../models/BeltInstance.model';
 import {HeadpieceInstance} from '../models/HeadpieceInstance.model';
+import {RingInstance} from '../models/RingInstance.model';
 
 export const isArmor = (armor: ItemInstance) => (armor as ArmorInstance).armorModel;
 export const isWeapon = (armor: ItemInstance) => (armor as WeaponInstance).weaponModel;
 export const isGlove = (item: ItemInstance) => (item as GloveInstance).gloveModel;
 export const isBelt = (item: ItemInstance) => (item as BeltInstance).beltModel;
 export const isHeadpiece = (item: ItemInstance) => (item as HeadpieceInstance).headpieceModel;
+export const isRing = (item: ItemInstance) => (item as RingInstance).ringModel;
