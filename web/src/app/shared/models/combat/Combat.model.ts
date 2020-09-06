17dd3ea126b8c31c8879301a264489aef7182cda
Ramiro <ramiro.tyszka@gmail.com>
2020-09-06 20:57:17 -0300

    #71 quick fixes

--- web/src/app/shared/models/combat/Combat.model.ts
+++ web/src/app/shared/models/combat/Combat.model.ts
@@ -3,10 +3,11 @@ import {Monster} from '../Monster.model';
 import {Hero} from '../NewHero.model';
 import {Initiative} from '../Iniciative.model';
 import {CombatLog} from './CombatLog';
+import {Creature} from '../creatures/Creature.model';
 
 export class Combat extends Entity {
-  monsters: Array<Monster> = new Array<Monster>();
-  heroes: Array<Hero> = new Array<Hero>();
+  monsters: Array<Monster | Creature> = new Array<Monster>();
+  heroes: Array<Hero | Creature> = new Array<Hero>();
   initiatives: Array<Initiative> = new Array<Initiative>();
   hasStarted: boolean;
   currentInitiative: Initiative = new Initiative();
