0acf90f6933c4b4eff42bd0d89aed7a0c5b39f4a
RMTTYszka <ramiro.tyszka@gmail.com>
2024-11-13 16:57:57 -0300

    added hands to equipment

---
+++ PocketEdition/RoleRollsPocketEdition/Migrations/20241113163620_addedHandsToEquipment.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RoleRollsPocketEdition.Migrations
+{
+    /// <inheritdoc />
+    public partial class addedHandsToEquipment : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "HandsId",
+                table: "Equipment",
+                type: "uuid",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Equipment_HandsId",
+                table: "Equipment",
+                column: "HandsId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Equipment_ItemInstances_HandsId",
+                table: "Equipment",
+                column: "HandsId",
+                principalTable: "ItemInstances",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Equipment_ItemInstances_HandsId",
+                table: "Equipment");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Equipment_HandsId",
+                table: "Equipment");
+
+            migrationBuilder.DropColumn(
+                name: "HandsId",
+                table: "Equipment");
+        }
+    }
+}
