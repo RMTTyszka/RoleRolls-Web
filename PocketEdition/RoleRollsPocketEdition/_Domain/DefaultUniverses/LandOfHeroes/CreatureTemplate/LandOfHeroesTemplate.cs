c408bd9ca4e8e8c483d0281cb0f55c2ef90db2f2
Rmttyszka <ramiro.tyszka@gmail.com>
2024-12-29 10:57:09 -0300

    added more config for land of heroes

--- PocketEdition/RoleRollsPocketEdition/_Domain/DefaultUniverses/LandOfHeroes/CreatureTemplate/LandOfHeroesTemplate.cs
+++
@@ -1,7 +0,0 @@
-namespace RoleRollsPocketEdition._Domain.DefaultUniverses.LandOfHeroes.CreatureTemplate;
-
-public class LandOfHeroesTemplate
-{
-    public static Guid Id => Guid.Parse("985C54E0-C742-49BC-A3E0-8DD2D6CE2632");
-    public static string Name => "Land Of Heroes";
-}
