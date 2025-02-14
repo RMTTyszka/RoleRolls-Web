44acf373be4003d428e0913cc2bd787bbd6041b2
RMTTYszka <ramiro.tyszka@gmail.com>
2025-02-14 12:38:31 -0300

    x

---
+++ PocketEdition/RoleRollsPocketEdition/Migrations/20250214130756_archetypes.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RoleRollsPocketEdition.Migrations
+{
+    /// <inheritdoc />
+    public partial class archetypes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "CampaignTemplateId",
+                table: "Archetypes",
+                type: "uuid",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Archetypes_CampaignTemplateId",
+                table: "Archetypes",
+                column: "CampaignTemplateId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Archetypes_CampaignTemplates_CampaignTemplateId",
+                table: "Archetypes",
+                column: "CampaignTemplateId",
+                principalTable: "CampaignTemplates",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Archetypes_CampaignTemplates_CampaignTemplateId",
+                table: "Archetypes");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Archetypes_CampaignTemplateId",
+                table: "Archetypes");
+
+            migrationBuilder.DropColumn(
+                name: "CampaignTemplateId",
+                table: "Archetypes");
+        }
+    }
+}
