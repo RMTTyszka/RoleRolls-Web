e9a79085b6a5ac9c66355581216b582f51ebd81f
RMTTYszka <ramiro.tyszka@gmail.com>
2025-10-13 11:25:46 -0300

    x

---
+++ PocketEdition/RoleRollsPocketEdition/Migrations/20251013140310_Migration_20251013_110302.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RoleRollsPocketEdition.Migrations
+{
+    /// <inheritdoc />
+    public partial class Migration_20251013_110302 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_MinorSkillTemplates_AttributeTemplates_AttributeTemplateId",
+                table: "MinorSkillTemplates");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_SkillTemplates_AttributeTemplates_AttributeTemplateId",
+                table: "SkillTemplates");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_SkillTemplates_CampaignTemplates_CampaignTemplateId",
+                table: "SkillTemplates");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "CampaignTemplateId",
+                table: "SkillTemplates",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "uuid",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_MinorSkillTemplates_AttributeTemplates_AttributeTemplateId",
+                table: "MinorSkillTemplates",
+                column: "AttributeTemplateId",
+                principalTable: "AttributeTemplates",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_SkillTemplates_AttributeTemplates_AttributeTemplateId",
+                table: "SkillTemplates",
+                column: "AttributeTemplateId",
+                principalTable: "AttributeTemplates",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_SkillTemplates_CampaignTemplates_CampaignTemplateId",
+                table: "SkillTemplates",
+                column: "CampaignTemplateId",
+                principalTable: "CampaignTemplates",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_MinorSkillTemplates_AttributeTemplates_AttributeTemplateId",
+                table: "MinorSkillTemplates");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_SkillTemplates_AttributeTemplates_AttributeTemplateId",
+                table: "SkillTemplates");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_SkillTemplates_CampaignTemplates_CampaignTemplateId",
+                table: "SkillTemplates");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "CampaignTemplateId",
+                table: "SkillTemplates",
+                type: "uuid",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uuid");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_MinorSkillTemplates_AttributeTemplates_AttributeTemplateId",
+                table: "MinorSkillTemplates",
+                column: "AttributeTemplateId",
+                principalTable: "AttributeTemplates",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_SkillTemplates_AttributeTemplates_AttributeTemplateId",
+                table: "SkillTemplates",
+                column: "AttributeTemplateId",
+                principalTable: "AttributeTemplates",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_SkillTemplates_CampaignTemplates_CampaignTemplateId",
+                table: "SkillTemplates",
+                column: "CampaignTemplateId",
+                principalTable: "CampaignTemplates",
+                principalColumn: "Id");
+        }
+    }
+}
